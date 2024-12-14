19493e026f723373ada0c89d4de5a9e3e5299242
EwanCscl <ewan.czarny@epitech.eu>
2024-12-15 05:10:41 +0800

    Feat mirror object
    add 5 mirror who turn broken when the player shoot it

---
+++ Assets/script/mirror_script.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class mirror_script : MonoBehaviour
+{
+    private GameObject player;
+    public GameObject reflectionProb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            reflectionProb.SetActive(true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            reflectionProb.SetActive(false);
+    }
+}
---
+++ Assets/script/script_shoot_mirror.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class script_shoot_mirror : MonoBehaviour
+{
+    private GameObject bullet;
+    public Material Mirror_break;
+    public GameObject Mirror;
+    public GameObject reflectionProb;
+
+    public int matnb = 3;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bullet = GameObject.FindWithTag("Bullet");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider bullet)
+    {
+        if (bullet.gameObject.CompareTag("Bullet")) {
+            reflectionProb.SetActive(false);
+            Mirror.GetComponent<mirror_script>().enabled = false;
+            if (Mirror.GetComponent<MeshRenderer>().materials.Length > 1) {
+                Material[] materials = Mirror.GetComponent<MeshRenderer>().materials;
+                materials[matnb] = Mirror_break;
+                Mirror.GetComponent<MeshRenderer>().materials = materials;
+            } else
+                Mirror.GetComponent<MeshRenderer>().material = Mirror_break;
+        }
+    }
+}
